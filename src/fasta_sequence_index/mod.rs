7801ebe6bdcd53afa1d19663e171827cc861f9d3
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2022-03-04 10:56:25 +0200

    Build the fasta sequence index in parallel.

--- src/fasta_sequence_index/mod.rs
+++ src/fasta_sequence_index/mod.rs
@@ -1,10 +1,12 @@
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{BufWriter, Seek, SeekFrom, Write};
+use std::io::{BufWriter, Write};
 use std::os::unix::fs::FileExt;
 use std::path::Path;
 use std::slice;
 use bio::io::fasta;
+use crossbeam::channel;
+use crossbeam::thread::Scope;
 
 struct FileSlice {
     offset: u64,
@@ -21,7 +23,6 @@ impl FastaSequenceIndex {
         let reader =
             fasta::Reader::with_capacity(64 * 1024 * 1024, File::open(input_file).unwrap());
         let mut writer = BufWriter::with_capacity(64 * 1024 * 1024, File::create(tmp_file).unwrap());
-
         let mut index = HashMap::new();
 
         let mut offset = 0;
@@ -44,7 +45,42 @@ impl FastaSequenceIndex {
         }
     }
 
+    pub fn build_parallel<P1: AsRef<Path>, P2: AsRef<Path>>(input_file: P1, tmp_file: P2, scope: &Scope, channel_size: usize) -> Self {
+        let reader =
+            fasta::Reader::with_capacity(64 * 1024 * 1024, File::open(input_file).unwrap());
+        let mut writer = BufWriter::with_capacity(64 * 1024 * 1024, File::create(tmp_file).unwrap());
+        let (sender, receiver) = channel::bounded(channel_size);
 
+        // Reader thread.
+        scope.spawn(move |_| {
+            for record in reader.records() {
+                let record = record.unwrap();
+                sender.send(record).unwrap();
+            }
+        });
+
+        // Writer thread.
+        let writer_result = scope.spawn(move |_| {
+            let mut index = HashMap::new();
+            let mut offset = 0;
+            while let Ok(record) = receiver.recv() {
+                let len = record.seq().len();
+                writer.write_all(record.seq()).unwrap();
+                writer.write_all(&[b'\n']).unwrap();
+
+                let previous = index.insert(record.id().as_bytes().to_vec(), FileSlice {offset, len});
+                offset += u64::try_from(len).unwrap() + 1;
+                assert!(previous.is_none());
+            }
+            (writer, index)
+        });
+
+        let (writer, index) = writer_result.join().unwrap();
+        Self {
+            file: writer.into_inner().unwrap(),
+            index,
+        }
+    }
 
     pub fn get_sequence(&mut self, id: &Vec<u8>, output: &mut Vec<u8>) {
         let file_slice = self.index.get(id).unwrap();
--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,15 @@ struct Configuration {
     /// A fasta file containing the homopolymer-compressed reads.
     #[clap(long, parse(from_os_str))]
     hoco_reads: PathBuf,
+
+    /// The size of the buffers between threads.
+    #[clap(long, default_value = "32768")]
+    buffer_size: usize,
+
+    /// The number of compute threads to use for decompression.
+    /// Note that the input and output threads are not counted under this number.
+    #[clap(long, default_value = "1")]
+    threads: usize,
 }
 
 fn initialise_logging() {
@@ -49,11 +58,11 @@ fn main() {
 
     info!("Building reads sequence indices...");
     let (normal_sequence_index, hoco_sequence_index) = crossbeam::scope(|scope| {
-        let normal_sequence_index = scope.builder().name("normal_index_builder_thread".to_string()).spawn(|_| {
-            FastaSequenceIndex::build(configuration.normal_reads, normal_sequence_index_path)
+        let normal_sequence_index = scope.builder().name("normal_index_builder_thread".to_string()).spawn(|scope| {
+            FastaSequenceIndex::build_parallel(configuration.normal_reads, normal_sequence_index_path, scope, configuration.buffer_size)
         }).unwrap();
-        let hoco_sequence_index = scope.builder().name("hoco_index_builder_thread".to_string()).spawn(|_| {
-            FastaSequenceIndex::build(configuration.hoco_reads, hoco_sequence_index_path)
+        let hoco_sequence_index = scope.builder().name("hoco_index_builder_thread".to_string()).spawn(|scope| {
+            FastaSequenceIndex::build_parallel(configuration.hoco_reads, hoco_sequence_index_path, scope, configuration.buffer_size)
         }).unwrap();
 
         (normal_sequence_index.join().unwrap(), hoco_sequence_index.join().unwrap())
